6c4f3248f5df74c3b7d4e494edea05330a8c17fc
Luis Juan Centurión <ljuan@koomkin.com>
2021-02-19 16:32:52 -0600

    added OrderResource to the serrver

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # Custom Resources
+from endpoints.order.resource import OrderResource
 from endpoints.store.resource import StoreResource
 from endpoints.whoami.resource import WhoAmIResource
 from endpoints.product.resource import ProductResource
@@ -15,6 +16,7 @@ api = Api(app)
 CORS(app)
 # Resources(All the clases developed for GET, POST, UPDATE, PUT)
 api.add_resource(WhoAmIResource, '/')
+api.add_resource(OrderResource, '/order')
 api.add_resource(StoreResource, '/store')
 api.add_resource(ProductResource, '/product')
 api.add_resource(InventoryResource, '/inventory')
